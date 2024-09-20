686b09e7eb150e9bfa80e248cf487ab4b30c07be
Brandon Harrington <bdharrington0@outlook.com>
2024-09-20 13:36:26 -0500

    Add erosion parameters.

--- src/erosion/ErosionSimulation.java
+++ src/erosion/ErosionSimulation.java
@@ -6,11 +6,90 @@ import java.util.List;
 
 import simulation.Simulation;
 import simulation.SimulationListener;
-import simulation.parameters.DoubleParameter;
+import simulation.parameters.BooleanParameter;
+import simulation.parameters.FloatParameter;
+import simulation.parameters.IntegerParameter;
 import simulation.parameters.SimulationParameterGroup;
 import util.data.trees.DistinguishedTree;
 
 public class ErosionSimulation implements Simulation<ErosionSimulationMode> {
+
+    private DistinguishedTree<String, SimulationParameterGroup> parameters 
+        = new DistinguishedTree<>(
+            "Erosion",
+            List.of(
+                new DistinguishedTree<>(
+                    "Landslides",
+                    List.of(
+                        new DistinguishedTree<>(
+                            new SimulationParameterGroup.Builder()
+                                .addIntegerParameter(new IntegerParameter(
+                                    "Blur Radius", 
+                                    "BlRad", 
+                                    "The radius of effect for the given blur",
+                                    1,
+                                    1,
+                                    20))
+                                .build()),
+                        new DistinguishedTree<>(
+                            new SimulationParameterGroup.Builder()
+                                .addFloatParameter(new FloatParameter (
+                                    "Blur Strength",
+                                    "BlSt", 
+                                    "The strength of the blur to be applied",
+                                    1.0f,
+                                    0.0f,
+                                    10.0f))
+                                .build()),
+                        new DistinguishedTree<>(
+                            new SimulationParameterGroup.Builder()
+                                .addFloatParameter(new FloatParameter(
+                                    "Prominence Dependence", 
+                                    "PromDep", 
+                                    "Determines how much the blur should vary with prominence",
+                                    0.0f,
+                                    0.0f,
+                                    1.0f))
+                                .build()),
+                        new DistinguishedTree<>(
+                            "Megaslides",
+                            List.of(
+                                new DistinguishedTree<>(
+                                    new SimulationParameterGroup.Builder()
+                                        .addBooleanParameter(new BooleanParameter(
+                                            "Enable Megaslides",
+                                            "EnMegSld", 
+                                            "Determines whether megaslides should be enabled",
+                                            false,
+                                            false,
+                                            false))
+                                        .build())))
+                        )),
+                new DistinguishedTree<>(
+                    "Simulated Water",
+                    List.of(
+                        new DistinguishedTree<>(
+                            new SimulationParameterGroup.Builder()
+                                .addFloatParameter(new FloatParameter(
+                                    "Slope Dependence", 
+                                    "SlpDep", 
+                                    "To what degree slope controls erosion",
+                                    1.0f,
+                                    0.0f,
+                                    10.0f))
+                                .build()),
+                        new DistinguishedTree<>(
+                            new SimulationParameterGroup.Builder()
+                                .addFloatParameter(new FloatParameter(
+                                    "Confluence Factor",
+                                    "CnflFac",
+                                    "How much the confluence of multiple lows increases the output",
+                                    1.0f,
+                                    0.0f,
+                                    2.0f))
+                                .build())
+                    ))
+            ));
 
     private ErosionSimulationMode mode = ErosionSimulationMode.HEIGHT;
 
@@ -18,22 +97,12 @@ public class ErosionSimulation implements Simulation<ErosionSimulationMode> {
 
     @Override
     public DistinguishedTree<String, SimulationParameterGroup> getParameters() {
-        return new DistinguishedTree<String, SimulationParameterGroup>(
-            "Box Blur Factors",
-            List.of(
-                new DistinguishedTree<>(
-                    new SimulationParameterGroup.Builder()
-                        .addDoubleParameter(new DoubleParameter("Blur Radius", "BlRad", "The radius of effect for the given blur"))
-                        .build()),
-                new DistinguishedTree<>(
-                    new SimulationParameterGroup.Builder()
-                        .addDoubleParameter(new DoubleParameter("Blur Strength", "BlSt", "The strength of the blur to be applied"))
-                        .build())));
+        return parameters;
     }
 
     @Override
     public void play() {
-
+        
     }
 
     @Override
